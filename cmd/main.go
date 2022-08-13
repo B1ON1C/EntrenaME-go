00f957bb1561d0d407373ff6bb5bf97d8695aa78
Luis Luzuriaga Manero <luisluzu98@gmail.com>
2022-08-13 21:01:38 +0200

    [FUN] - Main first messages

--- cmd/main.go
+++ cmd/main.go
@@ -1,4 +1,14 @@
 package main
 
+import (
+	"log"
+	"time"
+)
+
 func main() {
+	log.Println("Starting EntrenaME Server")
+	timeElapsedStart := time.Now()
+
+	timeElapsedEnd := time.Since(timeElapsedStart)
+	log.Println("EntrenaME Served started in " + timeElapsedEnd.String())
 }
